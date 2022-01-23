e8fec18941e3db1dd8b41e47fd0864957b60149c
Juan David Jaramillo <djaramilloj1@gmail.com>
2022-01-23 18:47:00 -0500

    Adding logout and fixing AuthGuard redirect

--- src/app/components/layout/layout.component.ts
+++ src/app/components/layout/layout.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthenticationService } from 'src/app/services/authentication/authentication.service';
 
 @Component({
   selector: 'app-layout',
@@ -7,9 +9,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LayoutComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    // Services
+    private authenticationService: AuthenticationService
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  signOut() {
+    localStorage.removeItem('userToken');
+    localStorage.removeItem('userId');
+    this.router.navigate(['/auth']);
   }
 
 }
--- src/app/guards/logged-in-user.guard.ts
+++ src/app/guards/logged-in-user.guard.ts
@@ -1,17 +1,24 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
 import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoggedInUserGuard implements CanActivate {
+  constructor(private router: Router) {}
+
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
     const userId: number = parseInt(localStorage.getItem('userId'));
     const userToken: string = localStorage.getItem('userToken');
-    return !userId || !userToken ? false : true; 
+    if(!userId || !userToken) {
+      this.router.navigateByUrl('/auth');
+      return false;
+    } else {
+      return true;
+    }
   }
   
 }
